36ea4a65fe183f4895ba6633a6fb24028874d10a
Hiroshi Fukada <hiroshi@fukada.us>
2025-11-14 23:17:26 -0500

    OH YEAH

--- monitor.py
+++ monitor.py
@@ -1,25 +1,41 @@
 import RPi.GPIO as GPIO
 import os
-import requests
+import aiohttp
 import time
 import asyncio
 
 GPIO.setmode(GPIO.BCM)
+NINJA_PIN = 23
+COWBOY_PIN = 18
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COWBOY_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(NINJA_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 URL_PREFIX=os.getenv('CVN_SERVER_URL')
 
+
+async def fire_request(target):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(target) as response:
+            print(target, await response.text())
+
 async def watch_button(pin, target):
+    url = f'{URL_PREFIX}/{target}'
+    prev_state = False
+    background_tasks = set()
     while True:
         pressed = not GPIO.input(pin)
-        if pressed:
-            print(target, requests.get(f'{URL_PREFIX}/{target}'))
-        await asyncio.sleep(0.1)
+        if pressed and not prev_state:
+            firenforget = asyncio.create_task(fire_request(url))
+            background_tasks.add(firenforget)
+            firenforget.add_done_callback(background_tasks.discard)
+        else:
+            await asyncio.sleep(0.05)
+        prev_state = pressed
 
 
 async def main():
-    tasks = [asyncio.create_task(watch_button(23, 'cowboy')), asyncio.create_task(watch_button(18, 'ninja'))]
+    tasks = [asyncio.create_task(watch_button(COWBOY_PIN, 'cowboy')),
+             asyncio.create_task(watch_button(NINJA_PIN, 'ninja'))]
     await asyncio.gather(*tasks)
     
 
